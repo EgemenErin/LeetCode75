c4491e475e031b5b2d7a786ca3632ad334a65006
Egemen Erin <pixelsushidev@hotmail.com>
2024-09-11 16:16:12 +0200

    twosum

---
+++ Arrays/IsAnagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        sa, ta = {}, {}
+        for i in range(len(s)):
+            sa[s[i]] = 1 + sa.get(s[i], 0)
+            ta[t[i]] = 1 + ta.get(t[i], 0)
+        return sa == ta
+
+
